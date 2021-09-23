aa47b4bf874f0241868a7781a5fa0857eee9581d
AlonsoMarin <90979445+AlonsoMarin@users.noreply.github.com>
2021-09-23 08:50:06 -0600

    Add files via upload

---
+++ Veterinaria/Controllers/AcercaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Veterinaria.Models;
+
+namespace Veterinaria.Controllers
+{
+    public class AcercaController : Controller
+    {
+        private readonly ILogger<AcercaController> _logger;
+
+        public AcercaController(ILogger<AcercaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Veterinaria/Controllers/ServiciosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Veterinaria.Models;
+
+namespace Veterinaria.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        private readonly ILogger<ServiciosController> _logger;
+
+        public ServiciosController(ILogger<ServiciosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
