fa07aa9587b8c8e4dcd61795337193422420d899
jorgeHernandezO <85416809+jorgeHernandezO@users.noreply.github.com>
2021-09-25 20:05:05 -0600

    Trabajo en clase Segunda semana

---
+++ SistemaClinica/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaClinica.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id_cliente { get; set; }
+        public string Nombre {  get; set; }
+        public string Apellidos {  get; set; }
+        public string Email {  get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? Cita {  get; set; }
+        public string Telefono {  get; set; }
+    }
+}
