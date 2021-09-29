f26dff56a2abd8b3c9e28996ff5f159c7a8a86f9
AlonsoMarin <90979445+AlonsoMarin@users.noreply.github.com>
2021-09-28 21:32:14 -0600

    Add files via upload

---
+++ Veterinaria/Models/PacienteMascota.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Models
+{
+    public class PacienteMascota
+    {
+        [Key]
+        public int id { get; set; }
+        public string NombreMascota { get; set; }
+        public string Raza { get; set; }
+        public DateTime FechaNacimientoMascota { get; set; }
+        public string NombreDueno { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        public string Cedula { get; set; }
+
+
+    }
+}
