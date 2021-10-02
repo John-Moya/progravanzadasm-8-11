75525f160715bb1860ec97e9870d98c4c3b78ea3
Jose Garcia <jose_int77@outlook.com>
2021-10-01 23:37:20 -0600

    Agregar formulario SQLProductos

---
+++ RestauranteJGV/Data/RestauranteJGVContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RestauranteJGV.Models;
+
+namespace RestauranteJGV.Data
+{
+    public class RestauranteJGVContext : DbContext
+    {
+        public RestauranteJGVContext (DbContextOptions<RestauranteJGVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RestauranteJGV.Models.Producto> Producto { get; set; }
+    }
+}
