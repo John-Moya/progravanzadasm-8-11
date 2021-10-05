5f2ae854446d24e50216298b25a7dadfb738a2fc
Jose Fabricio Martinez Rojas <71559339+Fabluxes@users.noreply.github.com>
2021-10-05 08:24:39 -0600

    Tarea Veterinaria S3
    En este comit se actualiza el branch con la taere a de veterinaria

---
+++ Veterinaria/Controllers/CitasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Veterinaria.Data;
+using Veterinaria.Models;
+
+namespace Veterinaria.Controllers
+{
+    public class CitasController : Controller
+    {
+        private readonly VeterinariaContext _context;
+
+        public CitasController(VeterinariaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Citas.ToListAsync());
+        }
+
+        // GET: Citas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var citas = await _context.Citas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (citas == null)
+            {
+                return NotFound();
+            }
+
+            return View(citas);
+        }
+
+        // GET: Citas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Paciente,TipoDeMascota,NombreMascota,FechaDeCita,Descripcion,CODIGOunicoPaciente")] Citas citas)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(citas);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(citas);
+        }
+
+        // GET: Citas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var citas = await _context.Citas.FindAsync(id);
+            if (citas == null)
+            {
+                return NotFound();
+            }
+            return View(citas);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Paciente,TipoDeMascota,NombreMascota,FechaDeCita,Descripcion,CODIGOunicoPaciente")] Citas citas)
+        {
+            if (id != citas.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(citas);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitasExists(citas.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(citas);
+        }
+
+        // GET: Citas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var citas = await _context.Citas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (citas == null)
+            {
+                return NotFound();
+            }
+
+            return View(citas);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var citas = await _context.Citas.FindAsync(id);
+            _context.Citas.Remove(citas);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitasExists(int id)
+        {
+            return _context.Citas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Veterinaria/Models/Citas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Models
+{
+    
+    public class Citas
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string  Paciente { get; set; }
+
+        public string TipoDeMascota { get; set; }
+
+        public string NombreMascota { get; set; }
+
+        public DateTime FechaDeCita { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int CODIGOunicoPaciente { get; set; }
+
+       
+
+    }
+}
