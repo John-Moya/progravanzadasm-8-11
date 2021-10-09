693d0f67705ff19ca03b7a3b44059ea7c9cd67d0
Jose Garcia <jose_int77@outlook.com>
2021-10-09 00:50:16 -0600

    tarea semana3

---
+++ Vete/Data/VeteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vete.Models;
+
+namespace Vete.Data
+{
+    public class VeteContext : DbContext
+    {
+        public VeteContext (DbContextOptions<VeteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vete.Models.Cita> Cita { get; set; }
+    }
+}
